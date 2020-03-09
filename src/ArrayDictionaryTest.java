aefd59a416ff0f4eaa1b47e6c76683f27aa179fd
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-03-09 16:40:05 -0700

    commiting is free

--- src/ArrayDictionaryTest.java
+++ src/ArrayDictionaryTest.java
@@ -15,14 +15,26 @@ public class ArrayDictionaryTest {
 
     @Test
     public void remove() {
-        int testSize = 5;
-        ArrayDictionary dict = new ArrayDictionary(testSize);
-        assertTrue(dict.add(2, 82));
-        assertTrue(dict.add(4, 84));
-        assertTrue(dict.add(7, 87));
-        System.out.println(dict);
-        dict.remove(7);
-        System.out.println(dict);
+        int[] testSizes = {
+                0, 0, 0,
+                1, 1,
+                2, 2, 2, 2,
+                3, 3,
+        };
+
+        ArrayDictionary[] dictArray = new ArrayDictionary[testSizes.length];
+
+        for(int i = 0; i < testSizes.length; i++) {
+            dictArray[i] = new ArrayDictionary(testSizes[i]);
+        }
+
+//        ArrayDictionary dict = new ArrayDictionary();
+//        assertTrue(dict.add(2, 82));
+//        assertTrue(dict.add(4, 84));
+//        assertTrue(dict.add(7, 87));
+//        System.out.println(dict);
+//        dict.remove(7);
+//        System.out.println(dict);
         assertTrue(false);  // place holder
     }
 
